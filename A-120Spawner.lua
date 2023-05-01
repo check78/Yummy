021aa31ab1e929c1e729e99e3539530d01d0a208
48447843573334422 <121458898+check78@users.noreply.github.com>
2023-05-01 03:24:53 +0300

    Add files via upload

--- A-120Spawner.lua
+++ A-120Spawner.lua
@@ -25,7 +25,7 @@ Modl.Parent = workspace
 Modl.PrimaryPart = Eyes
 
 			local Attachment = Instance.new("Attachment", Eyes)
-            Attachment.Position = Vector3.new(0, 2, 0)
+             Attachment.Position = Attachment.Position+Vector3.new(0, 2, 0)
 
 			local EyesParticle = Instance.new("ParticleEmitter", Attachment)
 			EyesParticle.LightInfluence = 0
