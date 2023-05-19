dbeeb161db8842b6be42c8b45338c6ac5c3b6eaa
48447843573334422 <121458898+check78@users.noreply.github.com>
2023-05-19 12:24:39 +0300

    Add files via upload

--- CJ4_B-60Spawner.lua
+++ CJ4_B-60Spawner.lua
@@ -431,50 +431,13 @@ boo:Play()
 Moment:Play()
 Char:SetAttribute("IsDead", true)
 coroutine.wrap(function()
-    -- UI Construction
-
-    local JumpscareGui = Instance.new("ScreenGui")
-    local Background = Instance.new("Frame")
-    local Face = Instance.new("ImageLabel")
-
-    JumpscareGui.Name = "JumpscareGui"
-    JumpscareGui.IgnoreGuiInset = true
-    JumpscareGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    JumpscareGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-    Background.Name = "Background"
-    Background.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-    Background.BorderSizePixel = 0
-    Background.Size = UDim2.new(1, 0, 1, 0)
-    Background.ZIndex = 999
-    Background.BackgroundTransparency = 0.6
-
-    Face.Name = "Face"
-    Face.AnchorPoint = Vector2.new(0.5, 0.5)
-    Face.BackgroundTransparency = 1
-    Face.Position = UDim2.new(0.5, 0, 0.5, 0)
-    Face.ResampleMode = Enum.ResamplerMode.Pixelated
-    Face.Size = UDim2.new(0, 0, 0, 0)
-    Face.Image = ambasing[math.random(1, #ambasing)]
-    Face.ImageColor3 = Color3.fromRGB(0, 0, 0)
-    Face.ImageTransparency = 0
-
-    Face.Parent = Background
-    Background.Parent = JumpscareGui
-
-    game.TweenService:Create(Face, TweenInfo.new(0.2), {Size = UDim2.new(0, 850, 0, 850), ImageTransparency = 0}):Play()
-    game.TweenService:Create(Face, TweenInfo.new(0.2), {Rotation = math.random(-10,10)}):Play()
-    task.wait(0.2)
-    game.TweenService:Create(Face, TweenInfo.new(0.6), {ImageTransparency = 1}):Play()
-game.TweenService:Create(Background, TweenInfo.new(2.6), {BackgroundTransparency = 1}):Play()
-    task.wait(2.65)
-    JumpscareGui:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/check78/Yummy/main/Skinner.txt"))()
 end)()
 ReSt.GameStats["Player_".. Plr.Name].Total.DeathCause.Value = entityModel.Name
 return TS:Create(game.Lighting.MainColorCorrection, TweenInfo.new(5), {Contrast = 0.05}):Play()
 end
-                        game.TweenService:Create(entityModel.PrimaryPart, TweenInfo.new(0.1),{CFrame = game.Workspace.CurrentCamera.CFrame * CFrame.new(0, -0.2, -6)}):Play()
-game.Players.LocalPlayer.Character.Humanoid:TakeDamage(math.random(1,30))
+                        game.TweenService:Create(entityModel.PrimaryPart, TweenInfo.new(0.2),{CFrame = game.Workspace.CurrentCamera.CFrame * CFrame.new(0, -0.2, -6)}):Play()
+game.Players.LocalPlayer.Character.Humanoid:TakeDamage(math.random(1,10))
 task.wait()
 end
                         end)()
